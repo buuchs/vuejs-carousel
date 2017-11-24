ade4711f1b92460129a657362e482dbf7284b988
Unknown <alexandre.buchs@epitech.eu>
2017-11-24 10:59:15 +0100

    First slider with VueJs2

---
+++ src/components/Carousel/Carousel.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="Carousel">
+    <slot></slot>
+    <h1>{{ title }}</h1>
+    <carousel-slide>
+      <img src="http://lorempicsum.com/futurama/630/300/1" alt="">
+      salut les gens
+    </carousel-slide>
+    <carousel-slide>
+      Au revoir les gens
+      <img src="http://lorempicsum.com/futurama/630/300/1" alt="">
+    </carousel-slide>
+    <button class="carousel_nav carousel_next" @click.prevent="next">Suivant</button>
+    <button class="carousel_nav carousel_prev" @click.prevent="prev">Précédent</button>
+  </div>
+</template>
+
+<script>
+  import CarouselSlide from '@/components/Carousel/Slides/CarouselSlides'
+
+  export default {
+    name: 'Carousel',
+    data () {
+      return {
+        title: 'Crarousel',
+        index: 0,
+        slides: []
+      }
+    },
+    mounted () {
+      this.slides = this.$children
+      this.slides.forEach((slide, i) => {
+        slide.index = i
+      })
+    },
+    computed: {
+      slidesCount () { return this.slides.length }
+    },
+    components: {
+      CarouselSlide
+    },
+    methods: {
+      next () {
+        this.index++
+        if (this.index >= this.slidesCount) {
+          this.index = 0
+        }
+      },
+      prev () {
+        this.index--
+        if (this.index < 0) {
+          this.index = this.slidesCount - 1
+        }
+      }
+    }
+  }
+</script>
---
+++ src/components/Carousel/Slides/CarouselSlides.vue
@@ -0,0 +1,54 @@
+<template>
+  <transition name="slide">
+    <div v-show="visible">
+      index : {{ index }}
+      <slot></slot>
+    </div>
+  </transition>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        index: 0
+      }
+    },
+    computed: {
+      visible () {
+        return this.index === this.$parent.index
+      }
+    }
+  }
+</script>
+
+<style>
+  .slide-enter-active {
+    animation : slideIn .5s
+  }
+
+  .slide-leave-active {
+    animation : slideOut .5s;
+    position: absolute;
+    top:0;
+    left:0;
+  }
+
+  @keyframes slideIn {
+    from {
+      transform : translateX(100%)
+    }
+    to {
+      transform: translateX(0);
+    }
+  }
+
+  @keyframes slideOut {
+    from {
+      transform: translateX(0)
+    }
+    to {
+      transform: translateX(-100%);
+    }
+  }
+</style>
